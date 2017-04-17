4f81286a34f279b0218e35b22e1c04554d4280e4
yangjinhua <yangjinhua@ane56.com>
2017-04-17 10:49:55 +0800

    加入创建数据库和表的各项配置。

---
+++ app/src/main/java/com/ane/coolweather/core/App.java
@@ -0,0 +1,15 @@
+package com.ane.coolweather.core;
+
+import android.app.Application;
+
+import org.litepal.util.Const;
+
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
+
